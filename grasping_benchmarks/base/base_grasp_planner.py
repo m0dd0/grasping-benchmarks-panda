5bb3a20fff5117b031174931fe4e15f3fd62d7f8
Moritz Hesche <mo.hesche@gmail.com>
2022-12-11 20:46:11 +0100

    cleaned BaseGraspPlanner

--- grasping_benchmarks/base/base_grasp_planner.py
+++ grasping_benchmarks/base/base_grasp_planner.py
@@ -6,30 +6,46 @@ import json
 import math
 import os
 import time
+from typing import Dict
+from abc import abstractmethod
 
 import numpy as np
 from grasping_benchmarks.base.grasp import Grasp6D
 
+
 class CameraData:
-    rgb_img = None
-    depth_img = None
-    pc_img = None
-    seg_img = None
-    bounding_box = None
-    intrinsic_params = None
-    extrinsic_params = {'position': np.ndarray((3, 1), float), 'rotation': np.eye(3)}
+    def __init__(
+        self,
+        rgb_image: np.ndarray = None,  # rgb_img
+        depth_image: np.ndarray = None,  # depth_img
+        pointcloud: np.ndarray = None,  # pc_image
+        segmentation_image: np.ndarray = None,  # seg_image
+        bounding_box=None,
+        intrinsic_params=None,
+        extrinsic_params=None,
+    ):
+        self.rgb_image = rgb_image
+        self.depth_image = depth_image
+        self.pointcloud = pointcloud
+        self.segmentation_image
+        self.intrinsic_params = intrinsic_params
+        self.extrinsic_params = extrinsic_params
+
+        if self.extrinsic_params is None:
+            self.extrinsic_params = {
+                "position": np.ndarray((3, 1), float),
+                "rotation": np.eye(3),
+            }
 
 
 class BaseGraspPlanner(object):
-    """The base class for grasp planners
+    """The base class for grasp planners."""
 
-    """
-    def __init__(self, cfg:dict):
-        """
-        Parameters
-        ----------
-        cfg : dict
-            Dictionary of configuration parameters.
+    def __init__(self, cfg: Dict):
+        """Initializes the grasp planner.
+
+        Args:
+            cfg (Dict): Dictionary of configuration parameters.
         """
         self.cfg = cfg
         self._grasp_poses = []
@@ -38,41 +54,39 @@ class BaseGraspPlanner(object):
         self._grasp_offset = np.zeros(3)
 
     def reset(self):
+        """Sets the GraspPlanner back to its inital state. Especially removes all
+        graps poses.
+        """
         self.grasp_poses = []
         self._best_grasp = None
         self._camera_data = CameraData()
 
-    def plan_grasp(self, camera_data, n_candidates=1):
-        """Grasp Planner
-            Compute candidate grasp poses
+    @abstractmethod
+    def plan_grasp(self, camera_data: CameraData, n_candidates: int = 1):
+        """Computes the given number of grasp candidates from from the given
+        camera data.
 
         Args:
-            camera_data (obj): `CameraData`. Contains the data (img, camera params) necessary to compute the grasp poses
-            n_candidates (int): number of candidate grasp poses to return
-
-        Raises:
-            NotImplementedError: [description]
+            camera_data (CameraData): Contains the data to compute the grasp poses
+            n_candidates (int, optional): The number of grasp candidates to compute. Defaults to 1.
         """
+        raise NotImplementedError()
 
-        raise NotImplementedError
-
+    @abstractmethod
     def visualize(self):
-        """Plot the grasp poses
-        """
-
-        pass
+        """Plot the grasp poses"""
+        raise NotImplementedError()
 
     @property
     def grasp_poses(self):
         return self._grasp_poses
 
     @grasp_poses.setter
-    def grasp_poses(self, grasp_poses:list):
-        if len(grasp_poses) is 0:
-            self._grasp_poses = []
-
-        elif type(grasp_poses[0]) is not Grasp6D:
-            raise ValueError('Invalid grasp type. Must be `benchmark_grasping.grasp.Grasp6D`')
+    def grasp_poses(self, grasp_poses: List[Grasp6D]):
+        if not all([type(p) is Grasp6D for p in grasp_poses]):
+            raise ValueError(
+                "Invalid grasp type. Must be `benchmark_grasping.grasp.Grasp6D`"
+            )
 
         self._grasp_poses = grasp_poses
 
@@ -81,8 +95,10 @@ class BaseGraspPlanner(object):
         return self._best_grasp
 
     @best_grasp.setter
-    def best_grasp(self, best_grasp:Grasp6D):
+    def best_grasp(self, best_grasp: Grasp6D):
         if type(best_grasp) is not Grasp6D:
-            raise ValueError('Invalid grasp type. Must be `benchmark_grasping.grasp.Grasp6D`')
+            raise ValueError(
+                "Invalid grasp type. Must be `benchmark_grasping.grasp.Grasp6D`"
+            )
 
         self._best_grasp = best_grasp
