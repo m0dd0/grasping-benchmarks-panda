702c0e2247a723cc39c28377a0325d35b1bcba77
Moritz Hesche <mo.hesche@gmail.com>
2023-01-02 16:30:51 +0100

    formatted base grasp planner

--- grasping_benchmarks/base/base_grasp_planner.py
+++ grasping_benchmarks/base/base_grasp_planner.py
@@ -2,39 +2,35 @@
 # This software may be modified and distributed under the terms of the
 # LGPL-2.1+ license. See the accompanying LICENSE file for details.
 
-from typing import Dict, List
+from typing import Dict, List, Union
 from abc import abstractmethod
 
 import numpy as np
+from nptyping import NDArray, Float, Shape
 
-from grasping_benchmarks.base.grasp import Grasp6D
+from .grasp import Grasp6D
 
 
 class CameraData:
     def __init__(
         self,
-        # TODO use nptyping
-        rgb_image: np.ndarray = None,  # rgb_img
-        depth_image: np.ndarray = None,  # depth_img
+        rgb_img: np.ndarray = None,  # rgb_img
+        depth_img: np.ndarray = None,  # depth_img
         pointcloud: np.ndarray = None,  # pc_image
-        segmentation_image: np.ndarray = None,  # seg_image
-        bounding_box=None, # bounding box around target object ?
+        seg_img: np.ndarray = None,  # seg_image
+        bounding_box=None,
         intrinsic_params=None,
-        extrinsic_params=None,
+        extrinsic_params: Dict[
+            str, Union[NDArray[Shape["3,3"], Float], NDArray[Shape["3"], Float]]
+        ] = None,
     ):
-        self.rgb_image = rgb_image
-        self.depth_image = depth_image
+        self.rgb_img = rgb_img
+        self.depth_img = depth_img
         self.pointcloud = pointcloud
-        self.segmentation_image = segmentation_image
+        self.seg_img = seg_img
         self.bounding_box = bounding_box
         self.intrinsic_params = intrinsic_params
         self.extrinsic_params = extrinsic_params
-
-        if self.extrinsic_params is None:
-            self.extrinsic_params = {
-                "position": np.ndarray((3, 1), float), #empty array
-                "rotation": np.eye(3), # identity matrix
-            }
 
 
 class BaseGraspPlanner(object):
@@ -49,23 +45,21 @@ class BaseGraspPlanner(object):
         """
         self._cfg = cfg
         # maintains a list of the lastly proposed grasp candidates
-        self._grasp_poses = [] 
-        # containes the best grasp candidate of the last plan_grasp call
-        self._best_grasp = None
-        # contains the camera data send to the last plan_grasp call 
-        self._camera_data = CameraData()
-        # self._grasp_offset = np.zeros(3)
+        self._grasp_poses: List[Grasp6D] = None
+        # contains the camera data send to the last plan_grasp call
+        self._camera_data = None
 
     def reset(self):
         """Sets the GraspPlanner back to its inital state. Especially removes all
         graps poses.
         """
-        self._grasp_poses = []
-        self._best_grasp = None
-        self._camera_data = CameraData()
+        self._grasp_poses = None
+        self._camera_data = None
 
     @abstractmethod
-    def plan_grasp(self, camera_data: CameraData, n_candidates: int = 1):
+    def plan_grasp(
+        self, camera_data: CameraData, n_candidates: int = 1
+    ) -> List[Grasp6D]:
         """Computes the given number of grasp candidates from from the given
         camera data. When implementing this class it needs to set the self._best_grasp,
         self._camera_data and self._grasp_poses properties of its instance accordingly.
@@ -82,30 +76,18 @@ class BaseGraspPlanner(object):
         raise NotImplementedError()
 
     @property
-    def grasp_poses(self):
+    def grasp_poses(self) -> List[Grasp6D]:
         return self._grasp_poses
-
-    # @grasp_poses.setter
-    # def grasp_poses(self, grasp_poses: List[Grasp6D]):
-    #     if not all([isinstance(p, Grasp6D) for p in grasp_poses]):
-    #         raise ValueError(
-    #             "Invalid grasp type. Must be `benchmark_grasping.grasp.Grasp6D`"
-    #         )
-
-    #     self._grasp_poses = grasp_poses
 
     @property
     def best_grasp(self):
-        return self._best_grasp
-
-    # @best_grasp.setter
-    # def best_grasp(self, best_grasp: Grasp6D):
-    #     if type(best_grasp) is not Grasp6D:
-    #         raise ValueError(
-    #             "Invalid grasp type. Must be `benchmark_grasping.grasp.Grasp6D`"
-    #         )
-
-    #     self._best_grasp = best_grasp
+        if self._grasp_poses is None:
+            return None
+        best_grasp = self._grasp_poses[0]
+        for grasp in self._grasp_poses:
+            if grasp.score > best_grasp.score:
+                best_grasp = grasp
+        return best_grasp
 
     @property
     def camera_data(self):
