213a44dcd6daf2a52d99e399db148bec8be4f2fe
Moritz Hesche <mo.hesche@gmail.com>
2023-10-09 14:19:38 +0200

    adding ros setup to bashrc

--- docker/grconvnet/entrypoint.sh
+++ docker/grconvnet/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e # exit on first error
 
-echo "running ros_entrypoint.sh"
-
-# setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source $CATKIN_WS_DIR/devel/setup.bash
-
 # execute any command entered after the docker run command
 echo "execute command $@"
 exec "$@"
