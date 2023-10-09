f2ceb8b3ad558814a0062a0490717219cc10c3b4
Moritz Hesche <mo.hesche@gmail.com>
2023-10-09 13:29:58 +0200

    renaming entrypoint script

--- docker/grconvnet/entrypoint.sh
+++ docker/grconvnet/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # exit on first error
 
+echo "running ros_entrypoint.sh"
+
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 source $CATKIN_WS_DIR/devel/setup.bash
