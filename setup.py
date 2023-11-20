b38d350ac8645cde019ef5d698d8da2e7c91bba2
Moritz Hesche <mo.hesche@gmail.com>
2023-11-20 19:34:07 +0100

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,15 @@ from pathlib import Path
 import setuptools
 
 
-# get all files in ./grasping_benchmarks which end with a config file ending
-config_file_endings = [".xml", ".yaml", ".ini"]
-config_files = filter(
-    lambda f: f in config_file_endings,
-    (Path(__file__).parent / "grasping_benchmarks").rglob("*"),
-)
-config_files = list(config_files)
-
 setuptools.setup(
-    name="grasping-benchmarks",
+    name="grasping_benchmarks",
     version="0.0.0",
     # author="elena rampone",
     # author_email="elena.rampone@iit.it",
     packages=setuptools.find_packages(),
-    package_data={"grasping_benchmarks": config_files},
+    package_data={
+        "grasping_benchmarks": list(Path(__file__).parent.glob("**/*.{yaml,xml,ini"))
+    },
     python_requires=">=3",
     install_requires=["numpy", "nptyping"],  # requirements from the base classes
     extras_require={
@@ -32,12 +26,6 @@ setuptools.setup(
             "Pillow",
             "rospkg",
         ],
-        "contact_graspnet": [
-            "matplotlib",
-            "pyyaml",
-            "Pillow",
-            "ros_numpy @ git+https://github.com/m0dd0/ros_numpy@master",  # the public/original version is outdated and does not support new version of numpy
-            "scipy",
-        ],
+        "contact_graspnet": [],
     },
 )
