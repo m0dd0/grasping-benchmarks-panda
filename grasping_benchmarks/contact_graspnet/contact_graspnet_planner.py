d7c06a9002074729a082d2eba83b3b895646d9b0
Moritz Hesche <mo.hesche@gmail.com>
2023-11-20 19:34:27 +0100

    starting new grasp planner

--- grasping_benchmarks/contact_graspnet/contact_graspnet_planner.py
+++ grasping_benchmarks/contact_graspnet/contact_graspnet_planner.py
@@ -1,1 +1,18 @@
-import 
+from typing import List
+from pathlib import Path
+import sys
+
+from grasping_benchmarks.base import BaseGraspPlanner, CameraData, Grasp6D
+
+
+class ContactGraspnetPlanner(BaseGraspPlanner):
+    def __init__(self, contact_graspnet_repo_path: Path):
+        sys.path.append(str(contact_graspnet_repo_path))
+        # from contact_graspnet import ContactGraspnet
+
+        # self.contact_graspnet = ContactGraspnet()
+
+    def plan_grasp(
+        self, camera_data: CameraData, n_candidates: int = 1
+    ) -> List[Grasp6D]:
+        pass
