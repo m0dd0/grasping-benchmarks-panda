cb28c71d141905576a25fd34e96e982413aba869
Moritz Hesche <mo.hesche@gmail.com>
2023-11-20 14:34:11 +0100

    setting up new planner

--- docker/contact_graspnet/compile_pointnet_tfops.sh
+++ docker/contact_graspnet/compile_pointnet_tfops.sh
@@ -7,7 +7,9 @@ cd /home/ContactGraspnetBenchmark/contact_graspnet/orig/pointnet2/tf_ops/samplin
 nvcc -c -o tf_sampling_g.cu.o tf_sampling_g.cu ${CUDA_INCLUDE} ${TF_CFLAGS} -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 g++ -std=c++11 -shared -o tf_sampling_so.so tf_sampling.cpp 
- tf_sampling_g.cu.o ${CUDA_INCLUDE} ${TF_CFLAGS} -fPIC -lcudart ${TF_LFLAGS} ${CUDA_LIB}
+# tf_sampling_g.cu.o ${CUDA_INCLUDE} ${TF_CFLAGS} -fPIC -lcudart ${TF_LFLAGS} ${CUDA_LIB}
+
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
 # echo 'testing sampling'
 # python3 tf_sampling.py
