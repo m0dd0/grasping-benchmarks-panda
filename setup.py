31712d6488dbf2e4c98e268d6206cd458f447a63
Moritz Hesche <mo.hesche@gmail.com>
2023-12-19 09:54:35 +0000

    Update setup.py with new dependencies

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_data={"grasping_benchmarks": config_files},
     python_requires=">=3",
-    install_requires=[ # requirements from the base classes
-        "numpy", 
+    install_requires=[  # requirements from the base classes
+        "numpy",
         "ros_numpy @ git+https://github.com/m0dd0/ros_numpy@master",  # the original version is outdated and does not support new version of numpy
-    ],  
+    ],
     extras_require={
         "dev": ["black", "pylint"],
         "grconvnet": [
@@ -33,6 +33,24 @@ setuptools.setup(
             "nptyping",
             "Pillow",
             "rospkg",
+        ],
+        "contact_graspnet": [
+            "tensorflow==2.11.0",
+            "numpy",
+            "opencv-python",
+            "opencv-python-headless",
+            "pyyaml==5.3.1",
+            "Pillow",
+            "trimesh",
+            "scipy",
+            "pyrender==0.1.43",
+            "pyglet==1.5.9",
+            "tqdm",
+            "matplotlib",
+            "nptyping",
+            "appdirs",
+            "tabulate",
+            "scikit-image",
         ],
     },
 )
