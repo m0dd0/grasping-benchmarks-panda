c654e0b588214afbef61a32c8cd8c58c48cca1b2
Moritz Hesche <mo.hesche@gmail.com>
2024-01-10 11:10:57 +0100

    Add SE3DifGraspPlanner class to grasp planner module

---
+++ grasping_benchmarks/se3dif/se3dif_grasp_planner.py
@@ -0,0 +1,53 @@
+"""_summary_
+"""
+
+from pathlib import Path
+from typing import List
+
+import yaml
+
+from grasping_benchmarks.base import BaseGraspPlanner, CameraData, Grasp6D
+
+
+class SE3DifGraspPlanner(BaseGraspPlanner):
+    @classmethod
+    def from_config_file(cls, config_file: Path) -> "SE3DifGraspPlanner":
+        """Creates a new instance of the GRConvNetGraspPlanner from a config file.
+
+        Args:
+            config_file (Path): Path to the yaml config file. The yaml file must
+                contain the same parameters as the __init__ method.
+
+        Returns:
+            SE3DifGraspPlanner: The new instance
+        """
+        with open(config_file, "r") as f:
+            cfg = yaml.safe_load(f)
+
+        return cls(**cfg)
+
+    def __init__(
+        self,
+        cfg: dict,
+    ):
+        super(SE3DifGraspPlanner, self).__init__(cfg)
+
+    def plan_grasp(
+        self, camera_data: CameraData, n_candidates: int = 1
+    ) -> List[Grasp6D]:
+        """Computes the given number of grasp candidates from from the given
+        camera data.
+
+        Args:
+            camera_data (CameraData): Contains the data to compute the grasp poses
+            n_candidates (int, optional): The number of grasp candidates to compute. Defaults to 1.
+        """
+
+        self._camera_data = camera_data
+
+        return self._grasp_poses
+
+    def visualize(self):
+        """Plot the grasp poses"""
+
+        return fig
