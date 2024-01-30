5d2b7e6e8296e758f62e63561e339a4b60d10272
Moritz Hesche <mo.hesche@gmail.com>
2024-01-30 08:56:04 +0100

    some changes to docker env

--- .devcontainer/se3dif/postCreateCommand.sh
+++ .devcontainer/se3dif/postCreateCommand.sh
@@ -1,8 +1,6 @@
-cd /home/catkin_ws 
-rosdep update 
-rosdep install --from-paths src --ignore-src -y -r 
-catkin config --extend /opt/ros/noetic 
-catkin build 
-pip install -e /home/catkin_ws/src/grasping-benchmark-panda[se3dif]
-# for some reason the compile_pointnet_script needs to be run manually after the container was build, otherwise no grasps are detected
-# the reason for this needs to be investigated
+# cd /home/catkin_ws 
+# rosdep update 
+# rosdep install --from-paths src --ignore-src -y -r 
+# catkin config --extend /opt/ros/noetic 
+# catkin build 
+# python3 -m pip install -e /home/catkin_ws/src/grasping-benchmark-panda[se3dif]
