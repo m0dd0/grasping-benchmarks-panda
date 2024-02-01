880729558595639d1c006f6503fd5c03f130a0ee
Moritz Hesche <mo.hesche@gmail.com>
2024-02-01 10:04:30 +0000

    working devcontainer

--- .devcontainer/se3dif/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-# cd /home/catkin_ws 
-# rosdep update 
-# rosdep install --from-paths src --ignore-src -y -r 
-# catkin config --extend /opt/ros/noetic 
-# catkin build 
-# python3 -m pip install -e /home/catkin_ws/src/grasping-benchmark-panda[se3dif]
