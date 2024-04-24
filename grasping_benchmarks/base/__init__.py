31ace4bf5c71b7baa8d58d8b02a016112c42b177
Moritz Hesche <mo.hesche@gmail.com>
2024-04-24 12:30:00 +0000

    moved dataclasses into same file

--- grasping_benchmarks/base/__init__.py
+++ grasping_benchmarks/base/__init__.py
@@ -1,6 +1,4 @@
 from .grasp_planner_base import *
-from .grasp_data import *
-from .camera_data import *
 from .typing import *
 
 # we do not impoert the ros_converters here as there iport might fail in case the ROS environment is not available
--- grasping_benchmarks/base/grasp_planner_base.py
+++ grasping_benchmarks/base/grasp_planner_base.py
@@ -7,6 +7,30 @@ from abc import abstractmethod
 
 from grasping_benchmarks.base import Grasp, CameraData
 
+from dataclasses import dataclass
+
+import numpy as np
+
+
+@dataclass
+class CameraData:
+    rgb_img: np.ndarray = None  # NpArray["H, W, 3", np.uint8] = None
+    depth_img: np.ndarray = None  # NpArray["H, W", np.uint16] = None
+    pointcloud: np.ndarray = None  # NpArray["N, 3", np.float32] = None
+    seg_img: np.ndarray = None  # NpArray["H, W", np.uint8] = None
+    cam_intrinsics: np.ndarray = None  # NpArray["3, 3", np.float32] = None
+    cam_pos: np.ndarray = None  # NpArray["3", np.float32] = None
+    cam_rot: np.ndarray = None  # NpArray["3, 3", np.float32] = None
+
+
+@dataclass
+class Grasp:
+    position: np.ndarray  # NpArray["3", np.float32] = np.zeros(3)
+    rotation: np.ndarray  # NpArray["3, 3", np.float32] = np.eye(3)
+    width: float = 0.0
+    score: float = 0.0
+    ref_frame: str = "camera"
+
 
 class BaseGraspPlanner(object):
     """The base class for grasp planners."""
