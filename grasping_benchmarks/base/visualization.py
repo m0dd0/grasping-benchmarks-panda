7c93881739797ec31cce6433a1409bed044de785
Moritz Hesche <mo.hesche@gmail.com>
2024-05-08 14:52:59 +0000

    improving viuslization helper

--- grasping_benchmarks/base/visualization.py
+++ grasping_benchmarks/base/visualization.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Tuple
 
+import numpy as np
 import trimesh
 
 from grasping_benchmarks.base import CameraData, Grasp
@@ -9,7 +10,11 @@ from grasping_benchmarks.base.transformations import (
 
 
 def panda_gripper_trimesh(
-    hand_height=0.127, finger_height=0.05, gripper_width=0.105, segment_radius=0.01
+    hand_height: float = 0.127,
+    finger_height: float = 0.05,
+    gripper_width: float = 0.105,
+    segment_radius: float = 0.01,
+    color: Tuple[int, int, int] = (0, 0, 0),
 ) -> trimesh.Trimesh:
     """Creates a simple trimesh representation of the Panda gripper.
 
@@ -23,13 +28,13 @@ def panda_gripper_trimesh(
         trimesh.Trimesh: The trimesh representation of the Panda gripper.
     """
     hand_segment = trimesh.creation.cylinder(
-        raius=segment_radius,
+        radius=segment_radius,
         segment=(
             (0, 0, 0),
             (0, 0, hand_height - finger_height),
         ),
     )
-    horizontal_segment = trimesh.creation.cyliner(
+    horizontal_segment = trimesh.creation.cylinder(
         radius=segment_radius,
         segment=(
             (0, -gripper_width / 2, hand_height - finger_height),
@@ -51,19 +56,24 @@ def panda_gripper_trimesh(
         ),
     )
 
-    return trimesh.util.concatenate(
+    gripper = trimesh.util.concatenate(
         [hand_segment, horizontal_segment, left_finger, right_finger]
     )
+    gripper.visual.face_colors = color
+
+    return gripper
 
 
 def visualize_grasp_pointcloud(
-    camera_data: CameraData, grasps: List[Grasp]
+    camera_data: CameraData, grasps: List[Grasp], max_pointcloud_points: int = None
 ) -> trimesh.Scene:
     """Visualizes the given grasp candidates in the given camera data.
 
     Args:
         camera_data (CameraData): Contains the data to visualize the grasp poses
         grasps (List[Grasp]): The grasp candidates to visualize
+        max_pointcloud_points (int, optional): The maximum number of points to sample from
+            the pointcloud. Defaults to all points.
 
     Returns:
         trimesh.Scene: The scene containing the camera data and the grasp candidates
@@ -71,7 +81,14 @@ def visualize_grasp_pointcloud(
 
     scene = trimesh.Scene()
 
-    for point in camera_data.pointcloud:
+    # trimeshs pointloud functionality seems to be buggy: https://github.com/mikedh/trimesh/issues/946
+    # scene.add_geometry(trimesh.points.PointCloud(camera_data.pointcloud))
+
+    pointcloud = camera_data.pointcloud
+    if max_pointcloud_points is not None:
+        pointcloud = pointcloud.sample(max_pointcloud_points)
+
+    for point in pointcloud:
         scene.add_geometry(trimesh.primitives.Sphere(radius=0.02, center=point))
 
     for grasp in grasps:
@@ -80,5 +97,12 @@ def visualize_grasp_pointcloud(
             rotation_and_position_to_homogeneous_matrix(grasp.rotation, grasp.position)
         )
         scene.add_geometry(gripper_trimesh)
+
+    coordinate_system = trimesh.creation.axis(
+        origin_size=0.04,
+        axis_radius=0.01,
+        axis_length=1,
+    )
+    scene.add_geometry(coordinate_system)
 
     return scene
